f9c9032215b7f58c512860ad219253b69230d850
ortizvinicius <vinicius.oaw@gmail.com>
2016-08-12 10:00:36 -0300

    First commit

---
+++ Gulpfile.js
@@ -0,0 +1,49 @@
+// Aqui nós carregamos o gulp e os plugins através da função `require` do nodejs
+var gulp = require('gulp');
+var jshint = require('gulp-jshint');
+var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
+var rename = require('gulp-rename');
+var browserSync = require('browser-sync').create();
+var reload = browserSync.reload;
+ 
+var files = "./src/*.js";
+var filesTest = "./test/*";
+ 
+gulp.task('lint', function() {
+	gulp.src(files)
+	.pipe(jshint())
+	.pipe(jshint.reporter('default'));
+});
+ 
+gulp.task('dist', function() {
+	gulp.src(files)
+	.pipe(concat('./dist'))
+	.pipe(rename('elpmisEditor.min.js'))
+	.pipe(uglify())
+	.pipe(gulp.dest('./dist'));
+});
+
+gulp.task('test', function() {
+	gulp.src(files)
+	.pipe(concat('elpmisEditor.js'))
+	.pipe(gulp.dest('./test'));
+});
+
+gulp.task('server', function() {
+  browserSync.init({
+    server: {
+      baseDir: "./test"
+    }
+  });
+});
+ 
+gulp.task('default', ['lint', 'dist', 'test', 'server', 'watch']);
+
+gulp.task('reload', function(){
+	reload();
+});
+
+gulp.task('watch', function() {
+	gulp.watch([files, filesTest], ['lint', 'dist', 'test', 'reload']);
+});
